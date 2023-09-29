9250cf36c5e492d5bec2666b0486c61331f3abef
Derutron <fidg_1@yahoo.com>
2023-09-29 17:05:59 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import FilterNav from './components/FilterNav';
 function App() {
   const [todoList, setTodoList] = useState([]);
 
+  //function to add new todo item
   const addTodo = (userInput) => {
     const newTodo = {
       id: Date.now(),
@@ -18,8 +19,27 @@ function App() {
     }
     setTodoList(prevData => [newTodo,...prevData])
   }
+
+  //function to remove a todoitem by its ID
+  const removeTodoById = (id) => {
+    const todoItems = todoList.filter(todo => todo.id !== id);
+    setTodoList(todoItems);
+  }
+
+  //Function to set status
+  const toggleTodoStatus = (id) => {
+    setTodoList(
+      todoList.map(todo => {
+        if (todo.id === id) {
+          return {...todo, status: !todo.status}
+        }
+        return todo;
+      })
+    )
+  }
+
   return (
-    <div className="min-h-screen bg-[#2f2f2]">
+    <div className="min-h-screen bg-[#2f2f2] 1g:w-[60%] lg:mx-auto">
       <div className="h-52 px-5 pt-10 bg-no-repeat bg-cover bg-[url('./image/bg-mobile-light.jpg')]">
       <div className="flex justify-between items-center">
         <p className="text-3x1 font-semibold text-white">TODO</p>
@@ -29,7 +49,11 @@ function App() {
       </div>
       <div className='relative -top-8 rounded-md bg-white h-56 w-[90%] mx-auto'>
         <FilterNav />
-       {todoList.map(item => <TodoItem todoData={item} key={item.id} />)}
+       {todoList.map(item => <TodoItem
+        todoData={item}
+        deleteItem={removeTodoById}
+        statusUpdate={toggleTodoStatus}
+        key={item.id} />)}
         <div className='flex justify-between px-5 items-center h-14'>
           <p>4 items</p>
           <button type='button'>Clear Completed</button>
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { MdDeleteForever, /*MdCheck,*/ MdCancel } from "react-icons/md";
+import { MdDeleteForever, MdCheck, MdCancel } from "react-icons/md";
 
-const TodoItem = ({ todoData }) => {
+const TodoItem = ({ todoData, deleteItem, statusUpdate }) => {
   return (
     <div>
       <div className='h-14 px-5 flex items-center'>
         <div className='flex items-center justify-between w-full'>
-          <div className='flex items-center'>
-            <MdCancel className='text-blue-500 h-5 w-4 mr-3' />
+          <div className='flex items-center justify-between'>
+            {todoData.status ? <MdCheck onClick={() => statusUpdate(todoData.id)} className='text-purple-500 h-5 w-4 mr-3' />: <MdCancel onClick={() => statusUpdate(todoData.id)} className='text-blue-500 h-5 w-4 mr-3' />} 
             <p className='text-gray-500'>{todoData.text}</p>
           </div>
           <div>
-            <MdDeleteForever className='h-5 w-5 text-red-500' />
+            <MdDeleteForever onClick={() => deleteItem(todoData.id)} className='h-5 w-5 text-red-500' />
           </div>
         </div>
       </div>
